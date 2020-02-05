c3665df92e478e6309312075262fa0dfb11526cd
robertzinzun <robertzinzun@gmail.com>
2020-02-05 12:19:00 -0600

    Se agrego el componente para realizar la conexion con la BD

---
+++ AgroERP/src/datos/conexion.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datos;
+
+/**
+ *
+ * @author roberto
+ */
+public class conexion {
+    public conexion(){
+        System.out.println("Conectando con la BD....");
+    }
+}
